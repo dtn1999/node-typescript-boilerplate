33716b26970e9f192ac6b8fcce4967e0c7c024f8
Danyls Tchekambou Ngongang <danyngongang@gmail.com>
2020-11-29 11:13:58 +0100

    update boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-console.log("------------configured typescript node server 😀 ---------")
+/* eslint-disable no-unused-vars */
+console.log('------------configured typescript node server 😀 ---------');
 
-// typescript code 
-const addition:(a:number,b:number)=>number = (a,b)=>a+b;
+// typescript code
+const addition:(a:number, b:number)=>number = (a, b) => a + b;
 
 export default addition;
--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,17 +1,17 @@
+/* eslint-disable no-undef */
 import addition from '../src/index';
 
-describe('Test if configuration works',()=>{
-    
-    beforeAll(()=>{
-        console.log("----------- Tests are looding ----------------")
-    })
+describe('Test if configuration works', () => {
+  beforeAll(() => {
+    console.log('----------- Tests are looding ----------------');
+  });
 
-    afterAll(()=>{
-        console.log("----------- Tests are completed ----------------")
-    })
+  afterAll(() => {
+    console.log('----------- Tests are completed ----------------');
+  });
 
-    test('Function addion',()=>{
-        const result = addition(2,4);
-        expect(result).toBe(2+4);
-    })
-})
+  test('Function addion', () => {
+    const result = addition(2, 4);
+    expect(result).toBe(2 + 4);
+  });
+});
